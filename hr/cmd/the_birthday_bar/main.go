39f9fc09fc7387b5a856aec38763bcc115de09dc
hiagobalbino <hiagobalbinoo@gmail.com>
2024-02-17 22:34:49 -0300

    feat: finish the birthday bar exercise

--- hr/cmd/the_birthday_bar/main.go
+++ hr/cmd/the_birthday_bar/main.go
@@ -17,25 +17,24 @@ func main() {
 		{numberOfSquares: []int32{5, 5, 3, 2, 2, 2, 1, 2, 5, 3, 5, 5, 4, 3, 3, 5}, birthDay: 13, birthMonth: 3, expectedOutput: 3},
 	}
 
-	for _, input := range inputs {
+	for index, input := range inputs {
+		testNumber := index + 1
 		output := birthday(input.numberOfSquares, input.birthDay, input.birthMonth)
 		if input.expectedOutput != output {
-			panic(fmt.Sprintf("Want %d but got %d", input.expectedOutput, output))
+			panic(fmt.Sprintf("Test number %d want %d but got %d", testNumber, input.expectedOutput, output))
 		}
+		fmt.Printf("Test number %d: success\n", testNumber)
 	}
 }
 
 func birthday(s []int32, d int32, m int32) int32 {
-	if len(s) == 1 && s[0] == d {
-		return 1
-	}
-
 	var squares int32
-	for i := int32(0); i < int32(len(s))-m; i++ {
 
+	for i := int32(0); i < int32(len(s))-m+1; i++ {
 		var count int32
-		for j := i; j < i+m; j++ {
-			count += s[j]
+
+		for j := int32(0); j < m; j++ {
+			count += s[i+j]
 		}
 
 		if count == d {
